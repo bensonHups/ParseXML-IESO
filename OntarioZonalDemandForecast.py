780b2a0fedaf39f27e668f73dac5397561889e9e
bensonHups <benson@peakpowerenergy.com>
2017-06-11 13:57:58 -0400

    csv2day OntarioZonalDemandForecast

--- OntarioZonalDemandForecast.py
+++ OntarioZonalDemandForecast.py
@@ -108,7 +108,7 @@ def get_DataFrame_OntarioZonalDemand(filePath):
 
 
 file_folder='C:/Users/benson/Desktop/2016/Ontario Zonal Demand Forecast/'
-save_folder='C:/Users/benson/Desktop/IESO/2016/Ontario Zonal Demand Forecast/'
+csv_folder='C:/Users/benson/Desktop/2015-csv/Ontario Zonal Demand Forecast/'
 # C:\Users\benson\Desktop\2016\Predispatch Shadow Prices Report
 
 #generate all the fileList
@@ -148,7 +148,7 @@ def save_csv_OntarioZonalDemand(filename):
     t1=datetime.datetime.now()
     df = get_DataFrame_OntarioZonalDemand(filename)
     hour=(filename.split('/')[-1]).split('.')[0]
-    df.to_csv('%s%s.csv' % (save_folder, hour), header=True)
+    df.to_csv('%s%s.csv' % (csv_folder, hour), header=True)
     t2=datetime.datetime.now()
     print 'saved:%s,%s seconds'%(hour,t2-t1)
     time.sleep(2)
@@ -157,21 +157,22 @@ def save_csv_OntarioZonalDemand(filename):
 def print_result(request,result):
     print "result:%s %r"%(request.requestID,result)
 
-gen_file_list=generate_list_OntarioZonalDemand('20160621','20161231',file_folder)
-get_list_file=get_list_filename(file_folder,['.xml'])
-print len(get_list_file)
-used_list=[]
-for gen_str in gen_file_list:
-    flag=True
-    for get_str in get_list_file:
-        if gen_str==get_str:
-            flag=False
-    if flag==False:
-        used_list.append(gen_str)
-    if flag:
-        print gen_str
-print '--------------------------len:%i----------------'%len(used_list)
-save_csv_OntarioZonalDemand(used_list[0])
+def xml_day_parser(xml_folder):
+    gen_file_list=generate_list_OntarioZonalDemand('20160621','20161231',xml_folder)
+    get_list_file=get_list_filename(xml_folder,['.xml'])
+    print len(get_list_file)
+    used_list=[]
+    for gen_str in gen_file_list:
+        flag=True
+        for get_str in get_list_file:
+            if gen_str==get_str:
+                flag=False
+        if flag==False:
+            used_list.append(gen_str)
+        if flag:
+            print gen_str
+    print '--------------------------len:%i----------------'%len(used_list)
+    save_csv_OntarioZonalDemand(used_list[0])
 
 # t1=datetime.datetime.now()
 # print t1
@@ -179,3 +180,143 @@ save_csv_OntarioZonalDemand(used_list[0])
 # pool.map(save_csv_PredispAreaOpResShortfalls,used_list)
 # t2=datetime.datetime.now()
 # print t2-t1
+
+day_folder= 'C:/Users/benson/Desktop/day_data/2016/Ontario Zonal Demand Forecast/'
+
+def is_datetime_equal(t1,t2):
+    t=t1-t2
+    if t.seconds!=0:
+        return False
+    if t.days!=0:
+        return False
+    return True
+def hour_dif(t1,t2):
+    t=t1-t2
+    hour_t=t.days*24+(t.seconds)/(60*60)
+    return hour_t
+
+def get_csv_list(daystr,folder):
+    f_list=get_list_filename(folder,['.csv'])
+    day_list=[]
+    for file in f_list:
+        if file.find(daystr)>=0:
+            day_list.append(file)
+    return day_list
+
+def generate_OntarioZonalDemand_Table(daystr,header,days_forecast):
+    day_hourlist=pd.date_range('%s 00:00:00'%daystr,'%s 23:55:00'%daystr,freq='H')
+    dict = {}
+    data_list = []
+    for i in range(len(day_hourlist)):
+        dict['datetime']=day_hourlist[i]
+        for j in range(len(header)):
+            str=header[j]
+            dict[str]=None
+            for k in range(1,days_forecast+1,1):
+                dict['%s_DA%i'%(str,k)]=None
+        dict2 = {}
+        dict2.update(dict)
+        data_list.append(dict2)
+
+    df_new = pd.DataFrame.from_dict(data_list)
+    df_new=df_new.set_index('datetime')
+    return df_new
+def date_dif(t1,t2):
+    t=t1.date()-t2.date()
+    return t.days
+def update_dataframe_value(timestamp,creatat,name,value,dataframe):
+    h=date_dif(timestamp,creatat)
+    if h==0:
+        column_name=name
+        print '%s:%s-%s'%(timestamp,name,value)
+        dataframe.set_value(timestamp, column_name, value, takeable=False)
+    elif h<7 and h>0:
+        column_name='%s_DA%i'%(name,h)
+        print '%s:%s-%s' % (timestamp, column_name, value)
+        dataframe.set_value(timestamp, column_name, value, takeable=False)
+    return dataframe
+
+def get_list_filename(file_folder, FlagStr=[]):
+    import os
+    fileList = []
+    fileNames = os.listdir(file_folder)
+    if len(fileNames) > 0:
+        for file in fileNames:
+            if len(FlagStr) > 0:
+                if IsSubString(FlagStr, file):
+                    fileList.append(os.path.join(file_folder, file))
+            else:
+                fileList.append(os.path.join(file_folder, file))
+    if len(fileList) > 0:
+        fileList.sort()
+    return fileList
+
+def get_weekbefore_list(daystr,xml_folder):
+    t1=datetime.datetime.strptime(daystr,'%Y%m%d')
+    t2=t1-datetime.timedelta(hours=6*24)
+    time_list=pd.date_range(t2,t1,freq='D')
+    day_str=[]
+    for tday in time_list:
+        print tday
+        t_date=datetime.datetime.strftime(tday,'%Y%m%d')
+        day_str.append(t_date)
+    file_list=[]
+    for dstr in day_str:
+        f_list=get_csv_list(dstr,xml_folder)
+        for f in f_list:
+            file_list.append(f)
+    return file_list
+
+def time_index_dataframe(daystr):
+    t1=datetime.datetime.now()
+    print t1
+    csv_list=get_weekbefore_list(daystr,csv_folder)
+    day_time=datetime.datetime.strptime(daystr,'%Y%m%d')
+    print csv_list
+    df = pd.read_csv(csv_list[0])
+    headers = df.groupby(df['zone'])
+    headers_node = []
+    for head in headers:
+        headers_node.append(head[0])
+    headers=[]
+    for head_zone in headers_node:
+        headers.append('OntarioZonalDemandForecast_%s'%head_zone)
+    df_save=generate_OntarioZonalDemand_Table(daystr,headers,6)
+    # # --create a save data table--
+    for file in csv_list:
+        df=pd.read_csv(file)
+        for index in df.index:
+            str_zone = df.loc[index, ['zone']][0]
+            # str_prictType= df.loc[index, ['PriceType']][0]
+            # str_schedule_typeID=df.loc[index, ['ScheduleTypeID']][0]
+            str_name='OntarioZonalDemandForecast_%s'%str_zone
+            ctime = df.loc[index, ['CreatedAt']][0]
+            ctime_y = datetime.datetime.strptime(ctime, '%Y-%m-%dT%H:%M:%S')
+            dtime = df.loc[index, ['datetime']][0]
+            dtime_y = datetime.datetime.strptime(dtime, '%Y-%m-%d %H:%M:%S')
+            value=df.loc[index, ['EnergyMW']][0]
+            if date_dif(dtime_y,day_time)==0:
+                df_save=update_dataframe_value(dtime_y,ctime_y,str_name,value,df_save)
+        t2=datetime.datetime.now()
+        print t2
+    df_save.to_csv('%sPUB_OntarioZonalDemandForecast_%s.csv' % (day_folder,daystr))
+    t2=datetime.datetime.now()
+    print 'saved:%s'%(t2-t1)
+
+def csv_hour_data():
+    t1=datetime.datetime.now()
+    print t1
+    day_list=pd.date_range('2016-06-27 00:00:00','2016-12-31 23:00:00',freq='D')
+    day_str=[]
+    for day in day_list:
+        dstr=str(day).split(' ')[0]
+        dstr=dstr.replace('-','')
+        day_str.append(dstr)
+    time_index_dataframe(day_str[0])
+    # print day_str
+    # pool=multiprocessing.Pool(multiprocessing.cpu_count())
+    # pool.map(time_index_dataframe,day_str)
+    # t2=datetime.datetime.now()
+    # print t2-t1
+
+csv_hour_data()
